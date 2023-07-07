c9272150721cad137ef30ae7cac70b963e858414
Boss <kosevaa@mail.ru>
2023-07-07 20:24:20 +0300

    asdxfgvhbj

--- proect.py
+++ proect.py
@@ -7,7 +7,7 @@ height=500
 wrap.world.create_world(width,height)
 
 y=300
-x1=100
+x1=300
 x2=400
 
 plat=wrap.sprite.add("mario-items",25,y,"moving_platform1")
@@ -20,4 +20,9 @@ plat=wrap.sprite.add("mario-items",x1,y,"moving_platform1")
 plat=wrap.sprite.add("mario-items",x2,y,"moving_platform1")
 pac=wrap.sprite.add("pacman", 20,y-23,"enemy_blue_down1")
 a=wrap.sprite.get_x(pac)
-wrap.actions.move_to(pac,(x1-a)/2,y-50)
+pol=(x1-a)/2
+wrap.actions.move_to(pac,(x1-a)/2+a,y-23-pol)
+wrap.actions.move_to(pac,x1,y-23)
+
+wrap.actions.move_to(pac,(x2-x1)/2+x1,y-23-pol)
+wrap.actions.move_to(pac,x2,y-23)
