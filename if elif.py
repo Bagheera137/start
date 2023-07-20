790e8a8e7616e22c478ca8f9ba7a8d732420c346
Boss <kosevaa@mail.ru>
2023-07-20 20:06:15 +0300

    asdxfgvhbj

---
+++ if elif.py
@@ -0,0 +1,20 @@
+a = 6
+if a == 5:
+    print("пять")
+if a == 4:
+    print("четыре")
+if a == 3:
+    print("три")
+if a < 3 or a > 5:
+    print("неизвестно")
+print("конец программы")
+# if a == 5:
+#     print("пять")
+# elif a == 4:
+#     print("четыре")
+# elif a == 3:
+#     print("три")
+# elif a == 5:
+#     print("снова пять")
+# else:
+#     print("неизвестно")
