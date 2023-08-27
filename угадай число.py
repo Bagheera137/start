11e2a75f6facb956da78c42253a289088049cf19
Boss <kosevaa@mail.ru>
2023-08-27 10:08:57 +0300

    asdxfgvhbj

--- угадай число.py
+++ угадай число.py
@@ -1,9 +1,16 @@
 import random
 a=input("угадай число")
 a=int(a)
-b=random.randint(1,10)
-
+b=random.randint(50,50)
 while a!=b:
-    a = input("угадай число")
-    a = int(a)
-print("угадал")
+    while a<b:
+       a = input("моё число больше")
+       a = int(a)
+    while a>b:
+        a = input("моё число меньше")
+        a = int(a)
+    if a==b:
+        # a = input("угадай число")
+        # a = int(a)
+        print("угадал")
+
