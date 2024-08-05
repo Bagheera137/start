cf2f799d8d3d5ad6eeb22d9ae0084d3ad058bab5
Boss <kosevaa@mail.ru>
2024-08-05 13:31:01 +0300

    asdxfgvhbj

--- mario football.py
+++ mario football.py
@@ -1,24 +1,28 @@
 import wrap
 
-def car(x1,y1,x2,y2,x3,y3):
+def day_pass(name):
 
-    mario1=wrap.sprite.add("mario-1-small", x1, y1, "stand")
-    mario2=wrap.sprite.add("mario-1-small", x2, y2, "stand")
-    mario3=wrap.sprite.add("mario-1-small", x3, y3, "stand")
-
-    ball=wrap.sprite.add("mario-enemies", x2, y2, "beetle_blue_go")
-    wrap.actions.set_size_percent(ball, 50, 50, 0)
-    wrap.actions.move_to(ball, x1, y1, 900)
-    wrap.actions.move_to(ball, x3, y3, 900)
-    wrap.actions.move_to(ball, x1, y1, 900)
-    wrap.actions.move_to(ball, x2, y2, 900)
-    wrap.actions.move_to(ball, x3, y3, 900)
-
+    x=wrap.sprite.get_x(name)
+    y=wrap.sprite.get_y(name)
+    wrap.actions.move_to(ball,x,y, 900)
 
 wrap.world.create_world(400, 600, 900, 50)
 wrap.world.set_back_color(234, 100, 200)
 
-car(100,100,200,300,300,100)
+ball=wrap.sprite.add("mario-enemies", 200, 300, "beetle_blue_go")
+wrap.actions.set_size_percent(ball, 50, 50, 0)
+
+mario1=wrap.sprite.add("mario-1-small", 100, 100, "stand")
+mario2=wrap.sprite.add("mario-1-small", 200, 300, "stand")
+mario3=wrap.sprite.add("mario-1-small", 300, 100, "stand")
+mario4=wrap.sprite.add("mario-1-small", 100, 300, "stand")
+
+day_pass(mario2)
+day_pass(mario3)
+day_pass(mario1)
+day_pass(mario4)
+
+
 
 
 
--- погоня.py
+++ погоня.py
@@ -1,18 +1,25 @@
 import time
 from wrap import world, sprite, sprite_text, actions
-def pobeg(ugol,ras,text1):
-    text = sprite.add_text(text1, sprite.get_x(victim), sprite.get_top(victim) - 20)
-    time.sleep(0.5)
-    sprite.remove(text)
+
+
+def pobeg(ugol, ras,text):
+    addtext(text,victim)
     actions.set_size_percent(victim, 30, 30, 500)
     actions.set_angle(victim, ugol, 500)
     actions.move_at_angle_dir(victim, ras, 600)
     actions.set_size_percent(victim, 100, 100, 500)
-def pogonya(ras2,text2):
-    text = sprite.add_text(text2, sprite.get_x(hunter), sprite.get_top(hunter)-20)
+
+
+def pogonya(ras2, text):
+    addtext(text,hunter)
+    actions.move_at_angle_point(hunter, sprite.get_x(victim), sprite.get_y(victim), ras2, 500)
+
+def addtext(text2,anim):
+    text = sprite.add_text(text2, sprite.get_x(anim), sprite.get_top(anim) - 20)
     time.sleep(0.5)
     sprite.remove(text)
-    actions.move_at_angle_point(hunter, sprite.get_x(victim), sprite.get_y(victim), ras2, 500)
+
+
 
 world.create_world(400, 600, 900, 50)
 world.set_back_color(100, 200, 200)
@@ -20,10 +27,10 @@ world.set_back_color(100, 200, 200)
 victim = sprite.add("pacman", 300, 100, "player2")
 hunter = sprite.add("pacman", 100, 100, "enemy_blue_right1")
 
-pobeg(240,50,"я быстрее тебя")
-pobeg(150,150,"не догонишь")
-pogonya(56,"я догоню тебя")
-pogonya(94,"не убежишь")
-pobeg(90,100,"пока пока")
-pogonya(200,"стой!")
 
+pobeg(240, 50,"не догонишь")
+# pobeg(150, 150, "не догонишь")
+pogonya(56,"стой")
+# pogonya(94, "не убежишь")
+# pobeg(90, 100, "пока пока")
+# pogonya(200, "стой!")
